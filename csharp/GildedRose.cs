865dbac17d344baccee7b3311447637ee2a2eeaa
wayne byard <user00015@gmail.com>
2019-09-22 12:24:37 -0400

    Strategy

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections.Generic;
+using csharp.ItemStrategy.Strategies;
 
 namespace csharp
 {
     public class GildedRose
     {
-        IList<Item> Items;
+        private const string AgedBrie = "Aged Brie";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        private const string ConcertTickets = "Backstage passes to a TAFKAL80ETC concert";
+        private const string Conjured = "Conjured Mana Cake";
+
+        readonly IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -12,77 +18,43 @@ namespace csharp
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach (var item in Items)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
+                var updateSolver = new Models.ItemStrategy(item);
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
+                /*
+                 * First iteration: Simple chain of if else to supply each strategy in turn.
+                 *
 
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
+                if(item.Name == AgedBrie) updateSolver.Strategy = new AgedItemStrategy();
+                else if(item.Name == Sulfuras) updateSolver.Strategy = new LegendaryItemStrategy();
+                else if(item.Name == ConcertTickets) updateSolver.Strategy = new VariableItemStrategy();
+                else updateSolver.Strategy = new RegularItemStrategy();
+
+                 */
+
+
+                //This can be abstracted further into a Director or Rules pattern, where a layer of abstraction makes this determination for us instead of calculating it here.
+                switch (item.Name)
+                {
+                    case AgedBrie:
+                        updateSolver.Strategy = new AgedItemStrategy();
+                        break;
+                    case Sulfuras:
+                        updateSolver.Strategy = new LegendaryItemStrategy();
+                        break;
+                    case ConcertTickets:
+                        updateSolver.Strategy = new VariableItemStrategy();
+                        break;
+                    case Conjured:
+                        updateSolver.Strategy = new ConjuredItemStrategy();
+                        break;
+                    default:
+                        updateSolver.Strategy = new RegularItemStrategy();
+                        break;
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
+                updateSolver.UpdateQuality();
             }
         }
     }
---
+++ csharp/ItemStrategy/Strategies/VariableItemStrategy.cs
@@ -0,0 +1,31 @@
+﻿using csharp.Models;
+
+namespace csharp.ItemStrategy.Strategies
+{
+    public class VariableItemStrategy : IBaseStrategy
+    {
+        public void UpdateQuality(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality += 1;
+
+                if (item.Quality < 50)
+                {
+                    if (item.SellIn <= 0)
+                    {
+                        item.Quality = 0;
+                    }
+                    else if (item.SellIn <= 5)
+                    {
+                        item.Quality += 2;
+                    }
+                    else if (item.SellIn <= 10)
+                    {
+                        item.Quality += 1;
+                    }
+                }
+            }
+        }
+    }
+}
